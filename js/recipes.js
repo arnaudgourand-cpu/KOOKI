499fa2cd6f1fcbc8e10a34c23b1a4da8d9b61d79
arnaudgourand-cpu <arnaud.gourand@gmail.com>
2026-03-25 14:33:36 +0100

    Update recipes.js

--- js/recipes.js
+++ js/recipes.js
@@ -58,7 +58,6 @@ function renderRecipes() {
     h += `<div class="rcard" onclick="showDet(${idx})">
       ${r.photo ? `<img class="rcard-photo" src="${r.photo}" alt="${r.name}">` : `<div class="rcard-emoji"></div>`}
       <div class="rcard-grad"></div>
-      <span class="rcard-cat">${CATS[r.category]||'Plat'}</span>
       <button class="rcard-heart${r.fav?' fav':''}" onclick="event.stopPropagation();toggleFav(${idx})">
         <svg viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
