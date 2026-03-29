bbd5a99b2059a8f166079818e869c1bf77a3b7ed
arnaudgourand-cpu <arnaud.gourand@gmail.com>
2026-03-29 10:12:41 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -2,15 +2,41 @@
 const MONTHS = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
 const JOURS  = ['dimanche','lundi','mardi','mercredi','jeudi','vendredi','samedi'];
 const CATS   = { plat:'Plat', entree:'Entrée', dessert:'Dessert', pdej:'Petit-déj', snack:'Snack' };
-const FOODS  = {
-  'Légumes':            ['carotte','courgette','tomate','oignon','ail','salade','épinard','poivron','champignon','pomme de terre','brocoli','chou','poireau','aubergine'],
-  'Fruits':             ['pomme','poire','banane','orange','citron','fraise','framboise','raisin','pêche','abricot'],
-  'Viandes & Poissons': ['poulet','bœuf','porc','agneau','saumon','thon','crevette','cabillaud','dinde','jambon','lardons','bacon'],
-  'Produits laitiers':  ['lait','crème','beurre','fromage','yaourt','œuf','mozzarella','parmesan','gruyère'],
-  'Féculents':          ['pâtes','riz','pain','farine','semoule','quinoa','lentille','haricot','pois chiche'],
-  'Épicerie':           ['huile','sel','poivre','sucre','miel','sauce soja','moutarde','vinaigre','bouillon'],
+const FOODS = {
+  'Primeur - Légumes':    ['carotte','courgette','tomate','oignon','ail','salade','épinard','poivron','champignon','pomme de terre','brocoli','chou','poireau','aubergine','concombre','céleri','radis','navet','artichaut','fenouil','maïs','haricot vert','petits pois','asperge','courge'],
+  'Primeur - Fruits':     ['pomme','poire','banane','orange','citron','fraise','framboise','raisin','pêche','abricot','mangue','ananas','kiwi','pastèque','melon','cerise','prune','figue','grenade','myrtille'],
+  'Viandes & Charcuterie':['poulet','bœuf','porc','agneau','dinde','veau','lapin','canard','jambon','lardons','bacon','saucisse','chorizo','salami','sauciflard','merguez','steak','côte','rôti','escalope'],
+  'Poissonnerie':         ['saumon','thon','crevette','cabillaud','sole','dorade','bar','sardine','maquereau','truite','calamar','moule','huître','langouste','homard','poulpe','anchois'],
+  'Produits laitiers':    ['lait','crème','beurre','fromage','yaourt','œuf','mozzarella','parmesan','gruyère','camembert','brie','roquefort','comté','emmental','ricotta','mascarpone'],
+  'Boulangerie':          ['pain','baguette','brioche','croissant','farine','levure','biscotte','pain de mie','crackers','fougasse'],
+  'Épicerie salée':       ['pâtes','riz','semoule','quinoa','lentille','haricot','pois chiche','boîte','conserve','huile','sel','poivre','moutarde','vinaigre','bouillon','sauce','mayonnaise','ketchup','cornichon'],
+  'Épicerie sucrée':      ['sucre','miel','confiture','nutella','chocolat','caramel','sirop','compote','biscuit','gâteau','céréales'],
+  'Boissons':             ['eau','jus','soda','café','thé','lait végétal','vin','bière','cidre','sirop','smoothie','limonade'],
+  'Apéro':                ['chips','cacahuète','olive','tarama','houmous','guacamole','toast','gougère','nems','bouchée'],
+  'Desserts':             ['glace','sorbet','mousse','tarte','éclair','madeleine','tiramisu','panna cotta','crème brûlée','flan'],
+  'Hygiène & Soin':       ['shampoing','gel douche','savon','dentifrice','déodorant','crème','rasoir','coton','maquillage','parfum'],
+  'Maison & Entretien':   ['lessive','liquide vaisselle','éponge','essuie-tout','poubelle','produit ménager','balai','serpillière','papier toilette','aspirateur'],
+  'Animaux':              ['croquette','pâtée','litière','jouet animal','gamelle','cage','aquarium','os','friandise animal'],
 };
-const SICO = {'Légumes':'🥦','Fruits':'🍎','Viandes & Poissons':'🥩','Produits laitiers':'🧀','Féculents':'🌾','Épicerie':'🫙','Autre':'📦'};
+
+const SICO = {
+  'Primeur - Légumes':    '🥦',
+  'Primeur - Fruits':     '🍎',
+  'Viandes & Charcuterie':'🥩',
+  'Poissonnerie':         '🐟',
+  'Produits laitiers':    '🧀',
+  'Boulangerie':          '🥖',
+  'Épicerie salée':       '🫙',
+  'Épicerie sucrée':      '🍫',
+  'Boissons':             '🥤',
+  'Apéro':                '🫒',
+  'Desserts':             '🍮',
+  'Hygiène & Soin':       '🧴',
+  'Maison & Entretien':   '🧹',
+  'Animaux':              '🐾',
+  'Autre':                '📦',
+  'Ajoutés manuellement': '📝',
+};
 
 /* ══ STATE ══ */
 let R   = JSON.parse(localStorage.getItem('kk-r')||'[]');
